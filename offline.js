b41109af97bb996ce752c7ac3929c307002c39be
DCTMonkey <82366189+DCTMonkey@users.noreply.github.com>
2022-02-28 02:58:50 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1645987821,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/movvinggift-sheet0.png",
+		"images/movvinggift-sheet1.png",
+		"images/movvinggift-sheet2.png",
+		"images/movvinggift-sheet3.png",
+		"images/movvinggift-sheet4.png",
+		"images/movvinggift-sheet5.png",
+		"images/movvinggift-sheet6.png",
+		"images/text-sheet0.png",
+		"images/hint-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
